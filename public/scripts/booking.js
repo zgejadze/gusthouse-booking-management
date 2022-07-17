01921da311f28646c2d41262327f689aa77de3e6
zakaria <zgejadze@gmail.com>
2022-07-18 00:28:36 +0400

    started working on search field (front-end side)

--- public/scripts/booking.js
+++ public/scripts/booking.js
@@ -1,1 +1,27 @@
-const searchBtn = document.getElementById('search-btn')
+const searchBtn = document.getElementById("search-btn");
+const formFieldSectionElement = document.getElementById("form-field");
+
+function loadSearchView() {
+  formFieldSectionElement.innerHTML = `
+  <form>
+    <p>
+        <label for="room-number">ოთახის ნომერი</label>
+        <select name="room-number" id="room-number">
+            <option>1-1</option>
+            <option value="">1-2</option>
+            <option value="">2-2</option>
+        </select>
+    </p>
+    <p>
+        <label for="date">თარიღი</label>
+        <input type="date" name="start-date" id="start-date">
+        <input type="date" name="end-date" id="end-date">
+
+    </p>
+
+    </form>
+`;
+    
+}
+
+searchBtn.addEventListener("click", loadSearchView);
