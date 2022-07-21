82b26a9f45d8294eed2103882d9d185485e31a81
zakaria <zgejadze@gmail.com>
2022-07-22 00:33:11 +0400

    finished working on quering active bookings on selected dates

--- models/booking.js
+++ models/booking.js
@@ -8,7 +8,7 @@ class Booking {
     this.room = bookingData.room;
     this.startDate = new Date(bookingData.startDate);
     this.endDate = new Date(bookingData.endDate);
-    this.id = bookingData._id
+    this.id = bookingData._id;
   }
 
   async save() {
@@ -24,24 +24,35 @@ class Booking {
   }
 
   static async lookForFreeRooms(bookingStarts, bookingEnds) {
-    const startDate = new Date(bookingStarts)
-    const endDate = new Date(bookingEnds)
-    
-    const query ={
-      $or: [{
-        startDate: { $gt: startDate, $lt: endDate},
-        endDate: { $gt: startDate, $lt: endDate }
-      }]
-    } 
+    const startDate = new Date(bookingStarts);
+    const endDate = new Date(bookingEnds);
+
+    const query = {
+      $or: [
+        {
+          startDate: {
+            $gt: startDate,
+            $lt: endDate,
+          },
+        },
+        {
+          endDate: {
+            $gt: startDate,
+            $lt: endDate,
+          },
+        },
+      ],
+    };
 
 
-    /// working query
-    // db.bookings.find({$or: [{'startDate': {$gte: new Date('2022-07-01'),$lt:new Date('2022-07-31')}}, {"endDate": {$gte: new Date('2022-07-01'),$lt:new Date('2022-07-31')}}]})
-
-    const result = await db.getDb().collection("bookings").find(query).toArray();
+    const result = await db
+      .getDb()
+      .collection("bookings")
+      .find(query)
+      .toArray();
     return result.map(function (result) {
       return new Booking(result);
-    })
+    });
   }
 }
 
