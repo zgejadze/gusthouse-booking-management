9dbde9efa5f7c5b84a0e50fd26826973cc9fa648
zakaria <zgejadze@gmail.com>
2022-07-22 00:21:13 +0400

    working on query

--- models/booking.js
+++ models/booking.js
@@ -24,11 +24,19 @@ class Booking {
   }
 
   static async lookForFreeRooms(bookingStarts, bookingEnds) {
+    const startDate = new Date(bookingStarts)
+    const endDate = new Date(bookingEnds)
     
-    const query = {
-      startDate: { $gt: bookingStarts, $lt: bookingEnds},
-      endDate: { $gt: bookingStarts, $lt: bookingEnds }
-    }
+    const query ={
+      $or: [{
+        startDate: { $gt: startDate, $lt: endDate},
+        endDate: { $gt: startDate, $lt: endDate }
+      }]
+    } 
+
+
+    /// working query
+    // db.bookings.find({$or: [{'startDate': {$gte: new Date('2022-07-01'),$lt:new Date('2022-07-31')}}, {"endDate": {$gte: new Date('2022-07-01'),$lt:new Date('2022-07-31')}}]})
 
     const result = await db.getDb().collection("bookings").find(query).toArray();
     return result.map(function (result) {
